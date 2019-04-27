2c5ed406ae536f45bc4d57c7390404d7a9ac8f35
Ryan Grosser <ryan.grosser91@gmail.com>
2019-04-28 09:37:06 +1000

    post requests with axios added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
@@ -7,6 +6,8 @@ const postsApi = 'http://localhost:3001/api/posts'
 
 class App extends Component {
   state = {
+    newPostTitle: '',
+    newPostBody: '',
     posts: []
   }
 
@@ -17,10 +18,41 @@ class App extends Component {
       })
   }
 
+  updateNewPostTitle = (event) => {
+    this.setState({
+      newPostTitle: event.target.value
+    })
+  }
+
+  updateNewPostBody = (event) => {
+    this.setState({
+      newPostBody: event.target.value
+    })
+  }
+
+  addPosts = (event) => {
+    event.preventDefault()
+    axios.post(postsApi, { title: this.state.newPostTitle, content: this.state.newPostBody })
+      .then((response) => {
+        const posts = [...this.state.posts, response.data]
+        this.setState({ posts, newPostTitle: '', newPostBody: '' })
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Posting</h1>
+        <h2>New Post</h2>
+        <form onSubmit={this.addPosts}>
+          <label>Title:</label><br /><input onChange={this.updateNewPostTitle} /><br />
+          <label>Content:</label><br /><input onChange={this.updateNewPostBody} /><br />
+          <input type="submit" value="Submit" />
+        </form>
+        <br />
+        <h2>Previous Posts</h2>
         { this.state.posts.map((post) => 
           <div>
             <h4>{ post.title }</h4>
