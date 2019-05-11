b75bd092d186eb176893bee0355b395566c69490
Ryan Grosser <ryan.grosser91@gmail.com>
2019-05-11 10:44:01 +1000

    Added basic signin component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import api from './api/init'
 import NewPost from './components/NewPost'
+import Signin from './components/Signin'
+import Notfound from './components/Notfound'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Post from './components/Post'
 
 
@@ -58,18 +61,24 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Posting</h1>
-        <h2>New Post</h2>
-        <form onSubmit={this.addPosts}>
-          <label>Title:</label><br /><input onChange={this.updateNewPostTitle} /><br />
-          <label>Content:</label><br /><input onChange={this.updateNewPostBody} /><br />
-          <input type="submit" value="Submit" />
-        </form>
-        <br />
-        <h2>Previous Posts</h2>
-        { this.state.posts.map((post) => 
-          <Post key={post._id} {...post} deletePost={this.deletePost} />
-        )}
+        <Router>
+          {/* <Route exact path="/login" component={Signin} /> */}
+
+          {/* <Route component={Notfound} /> */}
+
+          <h1>Posting</h1>
+          <h2>New Post</h2>
+          <form onSubmit={this.addPosts}>
+            <label>Title:</label><br /><input onChange={this.updateNewPostTitle} /><br />
+            <label>Content:</label><br /><input onChange={this.updateNewPostBody} /><br />
+            <input type="submit" value="Submit" />
+          </form>
+          <br />
+          <h2>Previous Posts</h2>
+          { this.state.posts.map((post) => 
+            <Post key={post._id} {...post} deletePost={this.deletePost} />
+          )}
+        </Router>
       </div>
     )
   }
