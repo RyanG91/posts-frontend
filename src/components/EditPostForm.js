9f6fd536d774de946fcfbd6b5df0f55d3cde58ee
Ryan Grosser <ryan.grosser91@gmail.com>
2019-05-27 14:40:29 +1000

    Added edit functions and posts can be edited

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -1,5 +1,19 @@
 import React from "react"
 
-export default function EditPostForm({  }) {
-
+export default function EditPostForm({ post, edit }) {
+  console.log(`post: ${post}, editPost: ${edit}`)
+  return (
+    <form onSubmit={edit}>
+      <label>
+        Title: <input type="text" name="title" defaultValue={post.title} />
+      </label>
+      <br />
+      <label>
+        Content: <input type="text" name="content" defaultValue={post.content} />
+      </label>
+      <br />
+      <input type="hidden" name="id" value={post._id} />
+      <button type="submit">Update</button>
+    </form>
+  )
 }
