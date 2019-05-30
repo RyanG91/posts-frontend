06f842c1db0ef8433182ea892bb366c421295cde
Ryan Grosser <ryan.grosser91@gmail.com>
2019-05-30 10:22:20 +1000

    Added button to return from edit screen and renamed edit function

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -1,19 +1,25 @@
 import React from "react"
 
-export default function EditPostForm({ post, edit }) {
-  console.log(`post: ${post}, editPost: ${edit}`)
+export default function EditPostForm({ post, editPosts, removeEdit }) {
+  // console.log(`post: ${post}, editPost: ${editPosts}`)
   return (
-    <form onSubmit={edit}>
-      <label>
-        Title: <input type="text" name="title" defaultValue={post.title} />
-      </label>
-      <br />
-      <label>
-        Content: <input type="text" name="content" defaultValue={post.content} />
-      </label>
-      <br />
-      <input type="hidden" name="id" value={post._id} />
-      <button type="submit">Update</button>
-    </form>
+    <div>
+      <form onSubmit={editPosts}>
+        <label>
+          Title: <input type="text" name="title" defaultValue={post.title} />
+        </label>
+        <br />
+        <label>
+          Content: <input type="text" name="content" defaultValue={post.content} />
+        </label>
+        <br />
+        <input type="hidden" name="id" value={post._id} />
+        <button type="submit">Update</button>
+      </form>
+      <form onSubmit={removeEdit}>
+        <button>Back</button>
+      </form>
+    </div>
+
   )
 }
