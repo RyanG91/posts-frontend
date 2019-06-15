9c0596a508267d31dde803f982b2e5addc862588
Ryan Grosser <ryan.grosser91@gmail.com>
2019-06-15 13:56:36 +1000

    Changed api

--- src/api/init.js
+++ src/api/init.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/'
+  baseURL: 'https://posting-server.herokuapp.com'
 })
 
 // Add the bearer token to the axios instance
