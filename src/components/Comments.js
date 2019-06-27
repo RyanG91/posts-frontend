db892941d0e3ef795629d8f3e59b6db42395ef3f
Ryan Grosser <ryan.grosser91@gmail.com>
2019-06-27 15:55:25 +1000

    Added additional styling and comment features

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,11 +3,20 @@ import "../styles/Comments.css"
 
 function Comments(props) {
   const { _id, body } = props
-  return (
-    <div className="commentsSection">
-      <p>{body}</p>
-    </div>
-  )
+  if (body === null) {
+    return (
+      <div className="commentsSection">
+        <p>There are no comments yet</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className="commentsSection">
+        <p>{body}</p>
+      </div>
+    )
+  }
+
 }
 
 export default Comments
