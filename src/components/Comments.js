1ecd00490a175e25ccf8960e376b21fbb094bb18
Ryan Grosser <ryan.grosser91@gmail.com>
2019-07-01 17:15:19 +1000

    Added author and created at for comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,7 +2,9 @@ import React from "react"
 import "../styles/Comments.css"
 
 function Comments(props) {
-  const { _id, body } = props
+  console.log('test')
+  const { _id, body, createdBy, createdAt } = props
+  // console.log(createdBy)
   if (body === null) {
     return (
       <div className="commentsSection">
@@ -12,6 +14,7 @@ function Comments(props) {
   } else {
     return (
       <div className="commentsSection">
+        <p><strong>{createdBy}</strong> commented at: {createdAt} </p>
         <p>{body}</p>
       </div>
     )
