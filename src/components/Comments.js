928d03ecd80b9226db61d25e1ce21210eb22f2d8
Ryan Grosser <ryan.grosser91@gmail.com>
2019-07-05 11:11:40 +1000

    Added function to delete comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,11 +1,17 @@
 import React from "react"
+import api from '../api/init'
+
 import "../styles/Comments.css"
+
+
 
 function Comments(props) {
 
-  const { _id, body, createdBy, createdAt } = props
+  const { _id, postId, body, createdBy, createdAt, likes, dislikes, deleteComment, addLikesComment } = props
 
   let commentTime = (new Date(createdAt)).toLocaleString('en-GB', { hour12: true })
+
+  console.log(postId)
 
   if (body === null) {
     return (
@@ -18,6 +24,14 @@ function Comments(props) {
       <div className="commentsSection">
         <p><strong>{createdBy}</strong> commented at: {commentTime} </p>
         <p>{body}</p>
+        <br />
+        <div>
+          <button >Edit Comment</button>
+          <button onClick={ () => {deleteComment(_id, postId)} } >Delete Comment</button>
+          {/* <button onClick={deleteComment} >Delete Post</button> */}
+        </div>
+
+        <br />
       </div>
     )
   }
